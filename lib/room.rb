1b086495191b4d478eab70ac9ba2f474a30b71bf
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-18 10:50:21 -0500

    write is_painted? and paint method, and painted, category, and demensions attributes

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,17 @@
 class Room
-attr_reader :category 
-  def initialize(:category)
+  attr_reader :category, :length, :width
+  attr_accessor :painted
+
+  def initialize(category, length, width)
     @category = :category 
+    @painted = false 
+  end
+
+  def is_painted?
+    @painted
+  end
+
+  def paint
+    @painted == true
   end
 end

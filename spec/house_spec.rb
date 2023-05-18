c5e9740db7f13578d08a86171b446a3bcd9e1b88
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-18 11:34:06 -0500

    add test to check house class attributes

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe House do
         house = House.new("$400000", "123 sugar lane")
         expect(house).to be_a House 
     end 
+
+    it "checks its attributes" do 
+        house = House.new("$400000", "123 sugar lane")
+        
+        expect(house.price).to eq(400000)
+        expect(house.address).to eq("123 sugar lane")
+        expect(house.rooms).to eq([])
+    end 
 end 

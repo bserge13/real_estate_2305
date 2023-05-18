572b4300928da9234e1c899cbab6bfa8d3781989
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-18 11:57:00 -0500

    add above_market_value method and psuedo code the 'how' behind it

--- lib/house.rb
+++ lib/house.rb
@@ -15,4 +15,13 @@ class House
     def add_room(room)
         @rooms << room 
     end
+
+    # The integer 500000 would require 
+    # argument of house price to be compared 
+    # as an integer: price(as integer) > 500000 
+    def above_market_value?
+        if price > 500000
+            true
+        end
+    end
 end

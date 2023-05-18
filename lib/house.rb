6c046952a6cf42561879b03cc306ec290da58db3
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-18 11:23:44 -0500

    initialize house class, test for its existance as instance, add attributes price, address, and rooms

---
+++ lib/house.rb
@@ -0,0 +1,10 @@
+class House 
+    attr_reader :price, :address
+    attr_accessor :rooms
+
+    def initialize(price, address)
+        @price = price
+        @address = address
+        @rooms = []
+    end
+end
